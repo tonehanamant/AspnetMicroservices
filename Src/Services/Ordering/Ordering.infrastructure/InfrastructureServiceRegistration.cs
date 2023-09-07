a4c0c3e7b26f64945f8bd38398b60b0cb58748dc
tHanmant Tone <hanmant.namdev.tone@P4ALAP-13373HN>
2023-09-07 21:32:51 +0530

    Added migration

---
+++ Src/Services/Ordering/Ordering.infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contract.Infrastructure;
+using Ordering.Application.Contract.Persistance;
+using Ordering.Application.Models;
+using Ordering.infrastructure.Mail;
+using Ordering.infrastructure.Persistance;
+using Ordering.infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
